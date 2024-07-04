33f98fd23e19451e9bf286b1e2accdc23bf7e49f
myaccount20232828 <146480147+myaccount20232828@users.noreply.github.com>
2024-07-03 21:23:24 -0400

    Update My App.swift

--- My App/My App.swift
+++ My App/My App.swift
@@ -2,20 +2,11 @@ import SwiftUI
 
 @main
 struct MyApp: App {
-    @State var Items: [String] = ["Sandboxed"]
     var body: some Scene {
         WindowGroup {
             Form {
-                ForEach(Items, id: \.self) { Item in
-                    Text(Item)
-                }
-            }
-            .onAppear {
-                do {
-                    Items = try FileManager.default.contentsOfDirectory(atPath: "/var")
-                } catch {
-                    print(error)
-                }
+                Text("visionOS App!")
+                    .foregroundColor(.indigo)
             }
         }
     }
