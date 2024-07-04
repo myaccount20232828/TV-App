a7c98f0f38d1ff898807adda41360e1f1acf53ee
myaccount20232828 <146480147+myaccount20232828@users.noreply.github.com>
2024-07-03 22:43:43 -0400

    Create tvOS.swift

---
+++ tvOS.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+
+@main
+struct MyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Form {
+                Text("tvOS App!")
+                    .foregroundColor(.red)
+                Button {
+                } label: {
+                  Text("Test")
+                }
+            }
+        }
+    }
+}
